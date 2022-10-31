85d08500486cd883f02a15dc0e3d1cd8cc70a014
shiqi <shiqi@mitake.com.tw>
2022-10-31 11:48:46 +0800

    建置cd deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  // publicPath 為 project-name，根目錄地址為上傳的網域
+  publicPath: '/shiqi/',
+};
